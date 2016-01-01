91c17814679524ede4c0c115421367b8e5a7f523
Leon Anavi <leon@anavi.org>
2016-01-02 01:58:08 +0200

    weather-station.js: basic integration with MQTT
    * Connect to MQTT test broker
    * Publish a message when temperature, humidity or pressure changes
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- weather-station.js
+++ weather-station.js
@@ -1,6 +1,9 @@
 var sys = require('sys')
 var process = require('child_process');
 
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://test.mosquitto.org');
+
 var python = "/usr/bin/python";
 var path = "/opt/rabbitpi/weather-station/";
 var cmdPrefix = python + " " + path;
@@ -13,11 +16,42 @@ var data = {
 		"humidity": 0
 }
 
+client.on('connect', function () {
+	client.subscribe('command');
+});
+
+client.on('message', function (topic, message) {
+	//TODO: disable print
+});
+
 function readData(error, stdout, stderr) {
 	if (null !== error) {
 		return false;
 	}
-	data = JSON.parse(stdout);
+	var dataNew = JSON.parse(stdout);
+
+	//check if weather data has changed
+	if (dataNew.temperature && data.temperature &&
+		(dataNew.temperature != data.temperature) ) {
+		client.publish('sensors/temperature',
+			JSON.stringify({ temperature: dataNew.temperature }) );
+	}
+
+	if (dataNew.humidity && data.humidity &&
+		(dataNew.humidity != data.humidity) ) {
+		client.publish('sensors/humidity',
+			JSON.stringify({ humidity: dataNew.humidity }) );
+	}
+
+	if (dataNew.pressure && data.pressure &&
+		(dataNew.pressure != data.pressure) ) {
+		client.publish('sensors/pressure',
+			JSON.stringify({ pressure: dataNew.pressure }) );
+	}
+
+	//update data
+	data = dataNew;
+
 	return true;
 }
 
